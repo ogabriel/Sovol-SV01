cd20ab6a7381cf2c07a12df671d5c19fbe29715d
ogabriel <gabrieloliver8991@gmail.com>
2022-09-23 21:30:09 -0300

    fix script

--- klipper/install_config.sh
+++ klipper/install_config.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "Sovol SV01" >> ~/klipper_config/printer.cfg
+echo "" >> ~/klipper_config/printer.cfg
+echo "# Sovol SV01" >> ~/klipper_config/printer.cfg
 echo "[include main.cfg]" >> ~/klipper_config/printer.cfg
 
 ln -sf ~/Sovol-SV01/klipper/main.cfg ~/klipper_config/main.cfg
 ln -sf ~/Sovol-SV01/klipper/software.cfg ~/klipper_config/software.cfg
 ln -sf ~/Sovol-SV01/klipper/hardware.cfg ~/klipper_config/hardware.cfg
-ln -sf ~/Sovol-SV01/klipper/macros/ ~/klipper_config/macros/
+ln -sf ~/Sovol-SV01/klipper/macros ~/klipper_config/macros
