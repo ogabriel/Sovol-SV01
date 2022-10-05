f24a356db4a1419938b791b820c590f63b500705
ogabriel <gabrieloliver8991@gmail.com>
2022-10-05 20:50:40 -0300

    add resonance to install_config

--- klipper/install_config.sh
+++ klipper/install_config.sh
@@ -7,4 +7,5 @@ echo "[include main.cfg]" >> ~/klipper_config/printer.cfg
 ln -sf ~/Sovol-SV01/klipper/main.cfg ~/klipper_config/main.cfg
 ln -sf ~/Sovol-SV01/klipper/software.cfg ~/klipper_config/software.cfg
 ln -sf ~/Sovol-SV01/klipper/hardware.cfg ~/klipper_config/hardware.cfg
+ln -sf ~/Sovol-SV01/klipper/resonance.cfg ~/klipper_config/resonance.cfg
 ln -sf ~/Sovol-SV01/klipper/macros ~/klipper_config/macros
