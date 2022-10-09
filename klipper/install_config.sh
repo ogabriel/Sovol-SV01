ae8fb3d8883af17d1471e88d3f051541a2e07ba7
ogabriel <gabrieloliver8991@gmail.com>
2022-10-09 11:32:36 -0300

    add new comments to install_config

--- klipper/install_config.sh
+++ klipper/install_config.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# show messages
 echo "" >> ~/klipper_config/printer.cfg
 echo "# Sovol SV01" >> ~/klipper_config/printer.cfg
 echo "[include main.cfg]" >> ~/klipper_config/printer.cfg
 
+# links files
 ln -sf ~/Sovol-SV01/klipper/main.cfg ~/klipper_config/main.cfg
 ln -sf ~/Sovol-SV01/klipper/software.cfg ~/klipper_config/software.cfg
 ln -sf ~/Sovol-SV01/klipper/hardware.cfg ~/klipper_config/hardware.cfg
