4f9c58fde39a0a7778ed7912d1dba06567ae3679
ogabriel <gabrieloliver8991@gmail.com>
2022-12-28 10:32:11 -0300

    add new calibration

--- klipper/calibration/extrude_rotation.rb
+++ klipper/calibration/extrude_rotation.rb
@@ -8,12 +8,5 @@ actual_extrude_distance = initial_mark_distance - subsequent_mark_distance
 
 previous_rotation_distance = BigDecimal("7.99058723472")
 rotation_distance = previous_rotation_distance * actual_extrude_distance / requested_extrude_distance
-rotation_distance.to_s
-rotation_distance.to_f
 
-# TUNING_TOWER COMMAND=SET_PRESSURE_ADVANCE PARAMETER=ADVANCE START=0 FACTOR=.005
-start = 0
-measured_height = BigDecimal("21.94")
-factor = BigDecimal("0.005")
-ressure_advance = 0 + measured_height * factor
-ressure_advance.to_f
+puts "extruder rotation_distance: #{rotation_distance.to_f}"
