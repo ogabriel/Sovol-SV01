973daaa6a6533f9240434b211b02a000b1389fed
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2023-03-12 18:07:27 -0300

    update install config

--- klipper/install_config.sh
+++ klipper/install_config.sh
@@ -7,4 +7,7 @@ echo "[include sv01/main.cfg]" >> ~/printer_data/config/printer.cfg
 echo "# [include sv01/resonance.cfg]" >> ~/printer_data/config/printer.cfg
 
 # links files
-ln -sf ~/Sovol-SV01/klipper/ ~/printer_data/config/sv01
+ln -sf ~/Sovol-SV01/klipper_config ~/printer_data/config/sv01
+
+# add crontab
+(crontab -l 2>/dev/null; echo "* * * * * cd /home/pi/Sovol-SV01; git pull") | crontab -
