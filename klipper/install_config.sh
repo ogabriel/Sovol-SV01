792a709710c1fa415716bf525a6d69d983518d82
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2023-03-12 17:43:06 -0300

    update script

--- klipper/install_config.sh
+++ klipper/install_config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # show messages
-echo "" >> ~/klipper_config/printer.cfg
-echo "# Sovol SV01" >> ~/klipper_config/printer.cfg
-echo "[include main.cfg]" >> ~/klipper_config/printer.cfg
+echo "" >> ~/printer_data/config/printer.cfg
+echo "# Sovol SV01" >> ~/printer_data/config/printer.cfg
+echo "[include main.cfg]" >> ~/printer_data/config/printer.cfg
 
 # links files
-ln -sf ~/Sovol-SV01/klipper/main.cfg ~/klipper_config/main.cfg
-ln -sf ~/Sovol-SV01/klipper/software.cfg ~/klipper_config/software.cfg
-ln -sf ~/Sovol-SV01/klipper/hardware.cfg ~/klipper_config/hardware.cfg
-ln -sf ~/Sovol-SV01/klipper/resonance.cfg ~/klipper_config/resonance.cfg
-ln -sf ~/Sovol-SV01/klipper/macros ~/klipper_config/macros
+ln -sf ~/Sovol-SV01/klipper/main.cfg ~/printer_data/config/main.cfg
+ln -sf ~/Sovol-SV01/klipper/software.cfg ~/printer_data/config/software.cfg
+ln -sf ~/Sovol-SV01/klipper/hardware.cfg ~/printer_data/config/hardware.cfg
+ln -sf ~/Sovol-SV01/klipper/resonance.cfg ~/printer_data/config/resonance.cfg
+ln -sf ~/Sovol-SV01/klipper/macros ~/printer_data/config/macros
