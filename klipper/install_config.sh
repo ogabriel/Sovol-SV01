e23171925939f1a6a509132b6359c9e69225d8ec
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2023-03-12 17:57:28 -0300

    install

--- klipper/install_config.sh
+++ klipper/install_config.sh
@@ -3,12 +3,8 @@
 # show messages
 echo "" >> ~/printer_data/config/printer.cfg
 echo "# Sovol SV01" >> ~/printer_data/config/printer.cfg
-echo "[include main.cfg]" >> ~/printer_data/config/printer.cfg
-echo "# [include resonance.cfg]" >> ~/printer_data/config/printer.cfg
+echo "[include sv01/main.cfg]" >> ~/printer_data/config/printer.cfg
+echo "# [include sv01/resonance.cfg]" >> ~/printer_data/config/printer.cfg
 
 # links files
-ln -sf ~/Sovol-SV01/klipper/main.cfg ~/printer_data/config/main.cfg
-ln -sf ~/Sovol-SV01/klipper/software.cfg ~/printer_data/config/software.cfg
-ln -sf ~/Sovol-SV01/klipper/hardware.cfg ~/printer_data/config/hardware.cfg
-ln -sf ~/Sovol-SV01/klipper/resonance.cfg ~/printer_data/config/resonance.cfg
-ln -sf ~/Sovol-SV01/klipper/macros ~/printer_data/config/macros
+ln -sf ~/Sovol-SV01/klipper/ ~/printer_data/config/sv01
