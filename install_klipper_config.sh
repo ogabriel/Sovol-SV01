5b12c91e5dc058479cfd338d57647993a45033f9
ogabriel <gabrieloliver8991@gmail.com>
2023-03-13 18:58:05 -0300

    add echo and crontab if

--- install_klipper_config.sh
+++ install_klipper_config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Adding link to printer.cfg"
+
 if ! grep -xF "[include sv01/main.cfg]" ~/printer_data/config/printer.cfg; then
 	echo "[include sv01/main.cfg]" >> ~/printer_data/config/printer.cfg
 fi
@@ -8,8 +10,10 @@ if ! grep -xF "# [include sv01/resonance.cfg]" ~/printer_data/config/printer.cfg
 	echo "# [include sv01/resonance.cfg]" >> ~/printer_data/config/printer.cfg
 fi
 
-# links files
+echo "Adding link to sv01 other files"
 ln -sf ~/Sovol-SV01/klipper_config ~/printer_data/config/sv01
 
-# add crontab
-(crontab -l 2>/dev/null; echo "* * * * * cd /home/pi/Sovol-SV01; git pull") | crontab -
+echo "Add crontab"
+if ! crontab -l | grep -F "* * * * * cd /home/pi/Sovol-SV01; git pull"; then
+	(crontab -l 2>/dev/null; echo "* * * * * cd /home/pi/Sovol-SV01; git pull") | crontab -
+fi
