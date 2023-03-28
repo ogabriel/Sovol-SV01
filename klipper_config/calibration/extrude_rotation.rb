611f83e47bc29eae8ac677f4cc01e079ed246788
ogabriel <gabrieloliver8991@gmail.com>
2023-03-27 21:00:25 -0300

    udpate

--- klipper_config/calibration/extrude_rotation.rb
+++ klipper_config/calibration/extrude_rotation.rb
@@ -2,13 +2,13 @@
 
 require 'bigdecimal/util'
 
-initial_mark_distance = BigDecimal("70.01")
+initial_mark_distance = BigDecimal("71.35")
 requested_extrude_distance = BigDecimal(50)
-subsequent_mark_distance = BigDecimal("20.11")
+subsequent_mark_distance = BigDecimal("20.76")
 
 actual_extrude_distance = initial_mark_distance - subsequent_mark_distance
 
-previous_rotation_distance = BigDecimal("7.99058723472")
+previous_rotation_distance = BigDecimal("23.88")
 rotation_distance = previous_rotation_distance * actual_extrude_distance / requested_extrude_distance
 
 puts "extruder rotation_distance: #{rotation_distance.to_f}"
